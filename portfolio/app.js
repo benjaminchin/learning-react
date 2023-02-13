d40814b1f7e3cf42f329fcf0366567dacfc7c28e
benjaminchin <benjaminchin@vt.edu>
2023-02-13 18:32:20 -0500

    back to top btn

---
+++ portfolio/app.js
@@ -0,0 +1,17 @@
+let btn = document.getElementById("btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.style.display = "block";
+    }
+    else {
+        btn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
